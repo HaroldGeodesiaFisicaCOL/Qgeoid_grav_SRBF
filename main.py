c28295b470e614ec63493b1b057b694c0a04c100
HaroldGeodesiaFisicaCOL <haroldlrt@gmail.com>
2026-07-31 08:42:34 -0500

    Initial commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # In this script, the first part consist in downloading the spherical harmonics coefficients of the
 # geopotential models, long wavelegth (GGM) and short wavelegth (Gtopo).
 import os
+from token import AMPEREQUAL
 import numpy as np
 from pathlib import Path
 
@@ -31,6 +32,10 @@ from modules.Download_spherical_harmonics.EGM_96.Expansion_EGM96 import (
 
 from modules.Remove_module.Terrestrial_processing.Main_Procesamiento_terrestres import (
     procesamiento_terrestres,
+)
+
+from modules.Remove_module.Aereal_processing.Main_Procesamiento_aereos import (
+    procesamiento_aereos,
 )
 
 #=============================================================================
@@ -150,8 +155,8 @@ expansion_EGM96(
 #=============================================================================
 # REMOVE STEP FOR THE GRAVITY OBSERVATIONS
 # First, the user have to know the number of technique's  gravity observations
-# For example, in the case of Italian model we have 2 techniques: Terrestrial and aereal.
-# for the colombian model we have 2 techniques: Terrestrial, aereal and altimetry.
+# For example, in the case of Italian model we have 2 techniques avalaible this repository: Terrestrial and aereal.
+# for the colombian model we have 3 techniques: Terrestrial, aereal and altimetry.
 #=============================================================================
 
 # For the terrestrial technique :
@@ -174,3 +179,25 @@ T_col_valor = r"gravity disturbance(mGal)"
 #     ruta_obs_final="modules/Compute_module/3_Observaciones/terrestrial_data.txt",
 #     graflab_path=str(graflab_path),
 # )
+
+# For the Airborne technique:
+# Include the column names for latitude, longitude, height, and value
+print('Removing airborne data...')
+A_col_lat = "Lat"
+A_col_lon = "Lon"
+A_col_h = "h"
+A_col_valor = r"gravity disturbance(mGal)"
+procesamiento_aereos(
+    input_file="modules/Remove_module/Initial_observations/airborne_data.txt",
+    col_lat=A_col_lat,
+    col_lon=A_col_lon,
+    col_h=A_col_h,
+    col_valor=A_col_valor,
+    path_gfc_model1="modules/Compute_module/1_Modelos/modeloXGM2019/XGM2019.gfc",
+    path_gfc_model2="modules/Compute_module/1_Modelos/modelo_dv_ell_Earth2014/dV_ELL_Earth2014_5480_plusGRS80.gfc",
+    path_gfc_model3="modules/Compute_module/1_Modelos/modelo_dv_ell_Earth2014/dV_ELL_Earth2014_5480_plusGRS80.gfc",
+    ruta_obs_final="modules/Compute_module/3_Observaciones/airborne_data.txt",
+    graflab_path=str(graflab_path),
+    filter_data=False,
+    subsampling=False,
+)
