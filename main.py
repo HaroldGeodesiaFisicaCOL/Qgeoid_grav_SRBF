aaa05d86505e791a3b4b5c950bf8a6ca39deed42
HaroldGeodesiaFisicaCOL <haroldlrt@gmail.com>
2026-08-03 15:48:17 -0500

    Initial commit

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from token import AMPEREQUAL
 import numpy as np
 from pathlib import Path
 
+from pandas.core.col import col
+
 
 from modules.Download_spherical_harmonics.Dowloand_grid_Model import (
     dowloand_all_folder,
@@ -189,20 +191,20 @@ A_col_lat = "Lat"
 A_col_lon = "Lon"
 A_col_h = "h"
 A_col_valor = r"gravity disturbance(mGal)"
-procesamiento_aereos(
-    input_file="modules/Remove_module/Initial_observation/airborne_data.txt",
-    col_lat=A_col_lat,
-    col_lon=A_col_lon,
-    col_h=A_col_h,
-    col_valor=A_col_valor,
-    path_gfc_model1="modules/Compute_module/1_Modelos/modeloXGM2019/XGM2019.gfc",
-    path_gfc_model2="modules/Compute_module/1_Modelos/modelo_dv_ell_Earth2014/dV_ELL_Earth2014_5480_plusGRS80.gfc",
-    path_gfc_model3="modules/Compute_module/1_Modelos/modelo_dv_ell_Earth2014/dV_ELL_Earth2014_5480_plusGRS80.gfc",
-    ruta_obs_final="modules/Compute_module/3_Observaciones/airborne_data.txt",
-    graflab_path=str(graflab_path),
-    filter_data=False,
-    subsampling=False,
-)
+# procesamiento_aereos(
+#     input_file="modules/Remove_module/Initial_observation/airborne_data.txt",
+#     col_lat=A_col_lat,
+#     col_lon=A_col_lon,
+#     col_h=A_col_h,
+#     col_valor=A_col_valor,
+#     path_gfc_model1="modules/Compute_module/1_Modelos/modeloXGM2019/XGM2019.gfc",
+#     path_gfc_model2="modules/Compute_module/1_Modelos/modelo_dv_ell_Earth2014/dV_ELL_Earth2014_5480_plusGRS80.gfc",
+#     path_gfc_model3="modules/Compute_module/1_Modelos/modelo_dv_ell_Earth2014/dV_ELL_Earth2014_5480_plusGRS80.gfc",
+#     ruta_obs_final="modules/Compute_module/3_Observaciones/airborne_data.txt",
+#     graflab_path=str(graflab_path),
+#     filter_data=False,
+#     subsampling=False,
+# )
 
 #=============================================================================
 # If you want include other type of gravity observation, example: Altimetry Data.
@@ -247,7 +249,7 @@ lon_min, lon_max, lat_min, lat_max = 11, 19, 39.5, 44.5
 ejecutar_pipeline_srbf(
     project_root=project_root,
 
-    freqs=["2190"],
+    freqs=["720"],
     resol="1",
 
     lat_min=lat_min,
@@ -255,7 +257,7 @@ ejecutar_pipeline_srbf(
     lon_min=lon_min,
     lon_max=lon_max,
 
-    apertura="0.326666666666666666666666666666",
+    apertura="0.1",
 
     puntos=(
         project_root
@@ -264,6 +266,9 @@ ejecutar_pipeline_srbf(
         / "3_Observaciones"
         / "terrestrial_data.txt"
     ),
+    obs_lat_col="Lat",
+    obs_lon_col="Lon",
+    obs_alt_col="h",
     puntos_aero=(
         project_root
         / "modules"
@@ -271,7 +276,9 @@ ejecutar_pipeline_srbf(
         / "3_Observaciones"
         / "airborne_data.txt"
     ),
-
+    aero_lat_col="Lat",
+    aero_lon_col="Lon",
+    aero_alt_col="h",
     cases=["1VC","1VCLC"],
 
     restore=True,
